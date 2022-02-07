c5cbcf74de14c84ed83ccf06548fde0f1242567c
Casey Spears <spearscase@gmail.com>
2022-02-06 21:38:10 -0700

    heroku bug still

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,13 +8,13 @@ router.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
 
+    router.use(express.static(path.resolve('../../frontend/build')));
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
         return res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
-    router.use(express.static(path.resolve('../../frontend/build')));
 
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
